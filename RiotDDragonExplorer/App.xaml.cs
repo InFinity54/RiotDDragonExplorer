897d27e64fddd9d3349e9753c51a3ed6354d0448
InFinity_54 <corentinkreicher@gmail.com>
2024-05-01 01:27:40 +0200

    Creating project

---
+++ RiotDDragonExplorer/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RiotDDragonExplorer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
